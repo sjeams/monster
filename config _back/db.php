dc6c710d69dcd0f54e695c7a7ba3f4dac70313b6
sjeams <359824901@qq.com>
2022-04-02 13:00:41 +0800

    撤回版本

--- config _back/db.php
+++ config _back/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=myapp',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=sumon',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'tablePrefix'=>'x2_'
 ];
